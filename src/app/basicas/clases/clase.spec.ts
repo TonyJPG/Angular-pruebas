1fd681a6bb4be0ac1006f198b0570a8b78c69944
Antonio Pérez <jpgtony@gmail.com>
2021-07-15 21:14:07 +0200

    Fin sección 22

--- src/app/basicas/clases/clase.spec.ts
+++ src/app/basicas/clases/clase.spec.ts
@@ -1,11 +1,21 @@
 import { Jugador } from './clase';
 describe('Pruebas de clase', () => {
-  const jugador = new Jugador();
+  let jugador = new Jugador();
 
   // beforeAll();
-  // beforeEach();
+
+  beforeEach(() => {
+    jugador = new Jugador();
+  });
+
   // afterAll();
+
   // afterEach();
+
+  it('Debe retornar 100 hp, si recibe 0 de daño', () => {
+    const resp = jugador.recibeDano(0);
+    expect(resp).toBe(100);
+  });
 
   it('Debe retornar 80 hp, si recibe 20 de daño', () => {
     const resp = jugador.recibeDano(20);
@@ -15,5 +25,15 @@ describe('Pruebas de clase', () => {
   it('Debe retornar 50 hp, si recibe 50 de daño', () => {
     const resp = jugador.recibeDano(50);
     expect(resp).toBe(50);
+  });
+
+  it('Debe retornar 0 hp, si recibe 100 de daño', () => {
+    const resp = jugador.recibeDano(100);
+    expect(resp).toBe(0);
+  });
+
+  it('Debe retornar 0 hp, si recibe  más de 100 de daño', () => {
+    const resp = jugador.recibeDano(300);
+    expect(resp).toBe(0);
   });
 });
