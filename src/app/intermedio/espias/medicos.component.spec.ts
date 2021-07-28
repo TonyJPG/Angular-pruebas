2ff4bedbc69f210709f6fea5365e36ed7f04dcd2
Antonio Pérez <jpgtony@gmail.com>
2021-07-28 23:57:52 +0200

    Fin clase 287 - Espías

--- src/app/intermedio/espias/medicos.component.spec.ts
+++ src/app/intermedio/espias/medicos.component.spec.ts
@@ -1,10 +1,27 @@
+import { from, of } from 'rxjs';
+
 import { MedicosComponent } from './medicos.component';
 import { MedicosService } from './medicos.service';
 
 describe('MedicosComponent', () => {
   let componente: MedicosComponent;
 
-  beforeEach(() => {});
+  const spy = jasmine.createSpyObj({ get: of({}) });
+  const servicio = new MedicosService(spy);
 
-  it('', () => {});
+  beforeEach(() => {
+    componente = new MedicosComponent(servicio);
+  });
+
+  it('Init: debe de cargar los médicos', () => {
+    const medicos = ['medico1', 'medico2', 'medico3'];
+
+    spyOn(servicio, 'getMedicos').and.callFake((): any => {
+      return from([medicos]);
+    });
+
+    componente.ngOnInit();
+    expect(componente.medicos?.length).toBeGreaterThan(0);
+    console.log(componente.medicos);
+  });
 });
