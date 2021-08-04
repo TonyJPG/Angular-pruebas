67b11806ed7034d7b546abd24e3d446f22de7123
Antonio Pérez <jpgtony@gmail.com>
2021-08-04 16:03:10 +0200

    Fin de video 300 - Pruebas de integracion, incrementador

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { MedicosComponent } from './intermedio/espias/medicos.component';
 import { MedicoComponent } from './intermedio2/medico/medico.component';
 import { HospitalComponent } from './intermedio2/hospital/hospital.component';
+import { IncrementadorComponent } from './intermedio2/incrementador/incrementador.component';
+
+import { MedicoService } from './intermedio2/medico/medico.service';
 
 @NgModule({
-  declarations: [AppComponent, MedicosComponent, MedicoComponent, HospitalComponent],
-  imports: [BrowserModule],
-  providers: [],
+  declarations: [
+    AppComponent,
+    MedicosComponent,
+    MedicoComponent,
+    HospitalComponent,
+    IncrementadorComponent,
+  ],
+  imports: [BrowserModule, FormsModule],
+  providers: [MedicoService],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
