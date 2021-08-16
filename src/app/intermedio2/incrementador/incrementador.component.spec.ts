1c8cb987ee1212d09bb9dcd9939e2c1b4379563b
Antonio Pérez <jpgtony@gmail.com>
2021-08-16 22:59:27 +0200

    Fin clase 302 - Pruebas del html del componente

--- src/app/intermedio2/incrementador/incrementador.component.spec.ts
+++ src/app/intermedio2/incrementador/incrementador.component.spec.ts
@@ -1,6 +1,7 @@
 import { TestBed, ComponentFixture } from '@angular/core/testing';
 import { IncrementadorComponent } from './incrementador.component';
 import { FormsModule } from '@angular/forms';
+import { By } from '@angular/platform-browser';
 
 describe('Incremendator Component', () => {
   let component: IncrementadorComponent;
@@ -16,7 +17,26 @@ describe('Incremendator Component', () => {
     component = fixture.componentInstance;
   });
 
-  it('es verdadero', () => {
-    expect(true).toBeTruthy();
+  it('Debe mostrar la leyenda en el h3', () => {
+    component.leyenda = 'Progreso de carga!';
+    fixture.detectChanges(); // dispara la detección de cambios
+
+    const elem: HTMLElement = fixture.debugElement.query(
+      By.css('h3')
+    ).nativeElement;
+
+    expect(elem.innerHTML).toContain('Progreso de carga!');
+  });
+
+  it('Debe mostrar en el input el valor de progreso', async () => {
+    component.cambiarValor(5);
+    fixture.detectChanges();
+
+    await fixture.whenStable();
+
+    const input = fixture.debugElement.query(By.css('input'));
+    const elem = input.nativeElement;
+
+    expect(elem.value).toBe('55');
   });
 });
