ca2ff8ae17b01eac59ab056664e2453457bea2ac
Antonio Pérez <jpgtony@gmail.com>
2021-08-17 16:46:49 +0200

    Fin clase 312 - Confirmar que existe un routerlink hacia una página especifica

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,6 +1,8 @@
 import { RouterTestingModule } from '@angular/router/testing';
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { By } from '@angular/platform-browser';
+import { RouterLinkWithHref, RouterOutlet } from '@angular/router';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
@@ -20,5 +22,32 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('pruebas');
+  });
+
+  it('Debe de tener un router-outlet', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const debugElement = fixture.debugElement.query(By.directive(RouterOutlet));
+
+    expect(debugElement).not.toBeNull();
+  });
+
+  it('Debe tener un link a la página de médicos', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const elementos = fixture.debugElement.queryAll(
+      By.directive(RouterLinkWithHref)
+    );
+
+    console.log(elementos);
+
+    let existe = false;
+
+    for (const elem of elementos) {
+      if (elem.attributes.routerLink === '/medicos') {
+        existe = true;
+        break;
+      }
+    }
+
+    expect(existe).toBeTrue();
   });
 });
